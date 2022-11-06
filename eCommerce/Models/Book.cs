b6da0442be25cc6801e867b6fbba376e9fd3af9d
RJ <24jrini@gmail.com>
2022-11-06 15:34:07 +0530

    Initial model - 1b

---
+++ eCommerce/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Models
+{
+    public class Book
+    {
+        [Key] public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public BookCategory bookCategory { get; set; }
+    }
+    public enum BookCategory
+    {
+        DataScience = 1,
+        Frontend,
+        FullStack
+    }
+}
