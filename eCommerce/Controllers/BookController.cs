46cc866a8176c6537c4d6095323f853cf8b01667
RJ <24jrini@gmail.com>
2022-11-08 13:10:44 +0530

    2e - commit

---
+++ eCommerce/Controllers/BookController.cs
@@ -0,0 +1,21 @@
+﻿using eCommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BookController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allPublications = await _context.Book.ToListAsync();
+            return View();
+        }
+    }
+}
---
+++ eCommerce/Controllers/PublicationController.cs
@@ -0,0 +1,21 @@
+﻿using eCommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Controllers
+{
+    public class PublicationController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PublicationController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allPublications = await _context.Publication.ToListAsync();
+            return View();
+        }
+    }
+}
